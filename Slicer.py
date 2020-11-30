962fba637527aa1f2a8b79b9180f8a711a41f4c0
anirudhprataps <69480660+anirudhprataps@users.noreply.github.com>
2020-11-30 11:22:13 +0530

    Update Slicer.py

--- Slicer.py
+++ Slicer.py
@@ -15,14 +15,7 @@ from drawlines import draw_lines
 Program designed to open and view ASCII STL files and then slice them for 3D printing operations. Supports variable 
 slice heights and spacing between infill grid lines. Geometry moved to fit within a 8x8x6 inch print bed as large as
 possible according to the part orientation.
-Output files consist of an SVG file for each slice of the model showing the model outlines and the infill pattern and a
-CSV file that lists the coordinates of the print head at different times and whether or not the extruder is on during
-the move to that position.
-
-Evan Chodora, 2018
-https://github.com/evanchodora/stl-slicer
-echodor@clemson.edu
-'''
+Output files consist of an SVG file for each slice of the model showing the model outlines and the infill pattern.
 
 
 # Class to draw an STL object from an ASCII STL file
